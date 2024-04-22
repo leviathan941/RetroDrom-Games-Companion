1b5027ef77fc85ff619cdb2ea19c0c91ae2eb913
Alexey Kuzin <amkuzink@gmail.com>
2024-04-22 21:33:12 +0200

    First coloring scheme try

---
+++ app/src/main/java/org/leviathan941/retrodromcompanion/ui/theme/CustomColor.kt
@@ -0,0 +1,26 @@
+/*
+ * RetroDrom Games Companion
+ * Copyright (C) 2024 Alexey Kuzin <amkuzink@gmail.com>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.leviathan941.retrodromcompanion.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DrawerLogoGradientStartColor = Color(0xFF28073E)
+val DrawerLogoGradientEndColor = Color(0xFF550A69)
+
+val RefreshClickableTextColor = Color(0xFF35A3DB)
