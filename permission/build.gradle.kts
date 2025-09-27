ce42de53022188d815e79905b8d4c395dce93241
Alexey Kuzin <amkuzink@gmail.com>
2025-09-27 15:47:42 +0200

    Bump third-party dependencies:
    * Compose Activity to 1.11.0,
    * Compose BOM to 2025.09.01,
    * Compose Material 3 to 1.4.0,
    * New library Compose Material Icons Core Android 1.7.8,
    * Firebase BOM to 34.3.0,
    * Google Material to 1.13.0.

--- permission/build.gradle.kts
+++ permission/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 dependencies {
     implementation(libs.accompanist.permissions)
     implementation(platform(libs.compose.bom))
+    implementation(libs.compose.material.icons.core.android)
     implementation(libs.compose.material3)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
