afd3cc826e1e53078b91957e67e9ad5307ece18d
Alexey Kuzin <amkuzink@gmail.com>
2026-09-13 11:26:32 +0200

    Bump app version to 1.12.0

--- buildSrc/src/main/kotlin/org/leviathan941/retrodromcompanion/AppVersion.kt
+++ buildSrc/src/main/kotlin/org/leviathan941/retrodromcompanion/AppVersion.kt
@@ -33,7 +33,7 @@ class AppVersion {
 
     companion object {
         const val MAJOR = "1"
-        const val MINOR = "11" // from 0 to 99
-        const val PATCH = "1" // from 0 to 99
+        const val MINOR = "12" // from 0 to 99
+        const val PATCH = "0" // from 0 to 99
     }
 }
